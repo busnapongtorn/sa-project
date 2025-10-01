8a853cd1d392b8f64f08e78c4ecf59a20b20e4f4
Napongtorn <napongtorn.s@ku.th>
2025-10-01 15:44:16 +0700

    initial commit

---
+++ src/test/java/cs/ku/sa_project/SaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.ku.sa_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
